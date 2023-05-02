30f42f228e965df97694a3bf9549ff7b8ba093cc
ErniNurkolis <erni.nurkolis@gmail.com>
2023-05-02 22:02:17 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -30,7 +30,7 @@ if (isset($_POST['but_upload'])) {
       if (in_array($imageFileType, $extensions_arr)) {
 
         // Insert record
-        $query = "UPDATE users SET profile_path = '$name' WHERE user_id='$userid'";
+        $query = "UPDATE users SET profile_path = '$names' WHERE userid='$userid'";
         mysqli_query($con, $query);
 
         // Upload file
