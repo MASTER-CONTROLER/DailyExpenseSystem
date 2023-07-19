e7541d6ce490ab32de389fb05b39af4a9ab4ccf0
MohammedR9 <remohammed@graduate.utm.my>
2023-07-19 15:46:13 +0800

    Updated Database File

--- DATABASE FILE/dailyexpense.sql
+++ DATABASE FILE/dailyexpense.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2021 at 07:16 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Jul 19, 2023 at 09:39 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `budgets`
+--
+
+CREATE TABLE `budgets` (
+  `budget_id` int(20) NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `amount` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `expenses`
 --
 
@@ -32,7 +46,65 @@ CREATE TABLE `expenses` (
   `expense` int(20) NOT NULL,
   `expensedate` varchar(15) NOT NULL,
   `expensecategory` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`expense_id`, `user_id`, `expense`, `expensedate`, `expensecategory`) VALUES
+(77, '7', 100, '2023-07-18', 'Entertainment'),
+(78, '7', 300, '2023-07-18', 'Food'),
+(79, '8', 100, '2023-07-18', 'Food'),
+(80, '7', 800, '2023-07-18', 'Clothings'),
+(81, '8', 200, '2023-07-18', 'Household Items'),
+(82, '7', 0, '2023-07-19', 'Medicine');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `notification_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `read` tinyint(1) NOT NULL DEFAULT 0,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`notification_id`, `user_id`, `message`, `read`, `date`) VALUES
+(1, 8, 'A new expense has been shared with you. Amount: $300', 0, '2023-07-19 15:06:11'),
+(2, 8, 'A new expense has been shared with you. Amount: $300', 0, '2023-07-19 15:06:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shared_expenses`
+--
+
+CREATE TABLE `shared_expenses` (
+  `shared_expense_id` int(20) NOT NULL,
+  `expense_id` int(20) NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  `amount` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `shared_expenses`
+--
+
+INSERT INTO `shared_expenses` (`shared_expense_id`, `expense_id`, `user_id`, `amount`) VALUES
+(1, 79, '7', 50),
+(2, 80, '8', 300),
+(3, 81, '7', 100),
+(4, 82, '8', 300),
+(5, 0, '8', 300);
 
 -- --------------------------------------------------------
 
@@ -48,17 +120,45 @@ CREATE TABLE `users` (
   `profile_path` varchar(50) NOT NULL DEFAULT 'default_profile.png',
   `password` varchar(50) NOT NULL,
   `trn_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `email`, `profile_path`, `password`, `trn_date`) VALUES
+(7, 'Ragab', 'Test', 'test@mail.com', 'Personal Photo.jpg', 'e10adc3949ba59abbe56e057f20f883e', '2023-07-18 11:55:20'),
+(8, 'test', 't1', 'test1@mail.com', 'default_profile.png', 'e10adc3949ba59abbe56e057f20f883e', '2023-07-18 13:03:42'),
+(9, 'Mohammed', 'Joe', 'mohammed1@mail.com', 'default_profile.png', 'fcea920f7412b5da7be0cf42b8c93759', '2023-07-19 09:09:25'),
+(10, 'Albraa', 'Joe', 'Albraa1@mail.com', 'default_profile.png', 'e10adc3949ba59abbe56e057f20f883e', '2023-07-19 09:09:58');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `budgets`
+--
+ALTER TABLE `budgets`
+  ADD PRIMARY KEY (`budget_id`);
+
+--
 -- Indexes for table `expenses`
 --
 ALTER TABLE `expenses`
   ADD PRIMARY KEY (`expense_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`notification_id`);
+
+--
+-- Indexes for table `shared_expenses`
+--
+ALTER TABLE `shared_expenses`
+  ADD PRIMARY KEY (`shared_expense_id`);
 
 --
 -- Indexes for table `users`
@@ -71,15 +171,36 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `budgets`
+--
+ALTER TABLE `budgets`
+  MODIFY `budget_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `expenses`
 --
 ALTER TABLE `expenses`
-  MODIFY `expense_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `expense_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `shared_expenses`
+--
+ALTER TABLE `shared_expenses`
+  MODIFY `shared_expense_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
